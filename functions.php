b272f3c4ec561687cd53391636fad36f6f39fd6a
Vitor Butkus <vitorbutkus@gmail.com>
2023-06-08 18:43:22 -0300

    s7l29

--- functions.php
+++ functions.php
@@ -3,25 +3,26 @@
 // function university_files() {
 //     wp_enqueue_style('university_main_styles', get_stylesheet_uri()); // adding styles from style.css
 
-function university_files() {
+function university_files()
+{
     wp_enqueue_script('main-university-js', get_theme_file_uri('build/index.js'), array('jquery'), 1.0, true); // adding the main js file; it takes 3 more arguments besides the file uri: dependecies, theme version, and a booloan which stands for if we want to load the file right after loading the head of the page.
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i'); // adding styles from google fonts CDN link (without the https: part)
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); // adding styles from fontawesome CDN link (without the https: part)
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css')); // adding styles from /build/style-index.css
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css')); // adding styles from /build/style-index.css
-    
-
-    
 
 }
 
 add_action('wp_enqueue_scripts', 'university_files'); // run university_files function before loading the head in html
 
-function university_features() {
+function university_features()
+{
     register_nav_menu('headerMenuLocation', 'Header Menu Location'); // register menu location so that in the CMS the user can create and edit menus (see index.php, the exact location is set there)
-    register_nav_menu('footerLocationOne', 'Footer Location 1'); 
-    register_nav_menu('footerLocationTwo', 'Footer Location 2'); 
+    register_nav_menu('footerLocationOne', 'Footer Location 1');
+    register_nav_menu('footerLocationTwo', 'Footer Location 2');
     add_theme_support('title-tag'); //enables dynamic titles on the page's tab
 }
 
 add_action('after_setup_theme', 'university_features');
+
+//see uint-university-theme/mu-plugins for other functions, eg university-post-types.php that defines custom post types (they are there so that the user can change the theme and keep having access to them in the CMS)
--- header.php
+++ header.php
@@ -1,48 +1,50 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-    <head>
-        <!-- ensures charset according to CMS: -->
-        <meta charset="<?php bloginfo('charset') ?> />
+
+<head>
+    <!-- ensures charset according to CMS: -->
+    <meta charset="<?php bloginfo('charset') ?> />
         <!-- ensures responsivity: -->
-        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
-        <!-- load page's head: -->
-        <?php wp_head(); ?>
-    </head>
-    <!-- generates classes for the body tag, dynamically (depending on which page is being rendered) -->
-    <body <?php body_class(); ?>>
+        <meta name=" viewport" content="width=device-width, initial-scale=1" />
+    <!-- load page's head: -->
+    <?php wp_head(); ?>
+</head>
+<!-- generates classes for the body tag, dynamically (depending on which page is being rendered) -->
+
+<body <?php body_class(); ?>>
     <header class="site-header">
-      <div class="container">
-        <h1 class="school-logo-text float-left">
-          <a href="<?php echo site_url('/'); ?>"><strong>Fictional</strong> University</a>
-        </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="container">
+            <h1 class="school-logo-text float-left">
+                <a href="<?php echo site_url('/'); ?>"><strong>Fictional</strong> University</a>
+            </h1>
+            <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+            <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
 
-        <div class="site-header__menu group">
+            <div class="site-header__menu group">
 
-          <nav class="main-navigation">
-            <!-- dynamic menu (created from CMS > Appearance > Menu): -->
-            <!-- <?php 
-                wp_nav_menu(array(
-                    'theme_location' => 'headerMenuLocation'
-                )); // associating this place with the location that is registered in functions.php
-            ?> -->
+                <nav class="main-navigation">
+                    <!-- dynamic menu (created from CMS > Appearance > Menu): -->
+                    <!-- <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'headerMenuLocation'
+                            )); // associating this place with the location that is registered in functions.php
+                            ?> -->
 
-            <!-- Static menu: -->
-            <ul>
-              <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 15) echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
-              <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
-              <li><a href="#">Campuses</a></li>
-              <li><a href="#">Blog</a></li>
-            </ul>
-          </nav>
+                    <!-- Static menu: -->
+                    <ul>
+                        <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 15) echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+                        <li><a href="#">Programs</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Campuses</a></li>
+                        <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"'; ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
+                    </ul>
+                </nav>
 
-          <div class="site-header__util">
-            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
-            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-          </div>
+                <div class="site-header__util">
+                    <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+                    <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+                    <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+                </div>
+            </div>
         </div>
-      </div>
     </header>
