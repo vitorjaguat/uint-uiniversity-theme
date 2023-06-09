3e7918ef88fc967e4d0d1dace73ffd25554cd194
Vitor Butkus <vitorbutkus@gmail.com>
2023-06-09 15:13:35 -0300

    creating relationships between different post-types in a theme

--- functions.php
+++ functions.php
@@ -27,6 +27,12 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query)
 {
+    if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'ASC');
+        $query->set('posts_per_page', -1);
+    }
+
     if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $today = date('Ymd');
         $query->set('meta_key', 'event_date');
